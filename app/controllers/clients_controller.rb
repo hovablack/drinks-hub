b51d07c4a9ce1d3c324e9668ba24bdfb3c588ccc
hovablack <nevermind92@hotmail.com>
2021-02-08 11:34:27 -0600

    begin registratio form

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,37 +1,53 @@
 class ClientsController < ApplicationController
+    
+    get '/register' do
+        erb :'clients/new.html'
+    end
 
-  # GET: /clients
-  get "/clients" do
-    erb :"/clients/index.html"
-  end
 
-  # GET: /clients/new
-  get "/clients/new" do
-    erb :"/clients/new.html"
-  end
+    post "/clients" do
+        raise params.inspect
+    end
 
-  # POST: /clients
-  post "/clients" do
-    redirect "/clients"
-  end
 
-  # GET: /clients/5
-  get "/clients/:id" do
-    erb :"/clients/show.html"
-  end
 
-  # GET: /clients/5/edit
-  get "/clients/:id/edit" do
-    erb :"/clients/edit.html"
-  end
+    private
 
-  # PATCH: /clients/5
-  patch "/clients/:id" do
-    redirect "/clients/:id"
-  end
+    def valid_input
+        params[:username] != "" && params[:email] != "" && params[:passowrd] != ""
+    end
 
-  # DELETE: /clients/5/delete
-  delete "/clients/:id/delete" do
-    redirect "/clients"
-  end
+
+
+    # GET: /clients
+    get "/clients" do
+        erb :"/clients/index.html"
+    end
+
+    # GET: /clients/new
+    get "/clients/new" do
+        erb :"/clients/new.html"
+    end
+
+    # POST: /clients
+
+    # GET: /clients/5
+    get "/clients/:id" do
+        erb :"/clients/show.html"
+    end
+
+    # GET: /clients/5/edit
+    get "/clients/:id/edit" do
+        erb :"/clients/edit.html"
+    end
+
+    # PATCH: /clients/5
+    patch "/clients/:id" do
+        redirect "/clients/:id"
+    end
+
+    # DELETE: /clients/5/delete
+    delete "/clients/:id/delete" do
+        redirect "/clients"
+    end
 end
